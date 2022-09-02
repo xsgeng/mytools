131b7c04deea29fd76edf33e4b1cdd094b5000e9
gengxs <xsgeng@siom.ac.cn>
2022-09-02 16:01:58 +0800

    a0 E0 and power conversion

--- lpi/laser.py
+++ lpi/laser.py
@@ -17,9 +17,18 @@ def I_from_E0(E0: float) -> float:
     return 0.5 * E0**2 / mu_0 / c
 
 
+def E0_from_I(I: float) -> float:
+    return sqrt(I*mu_0*c*2)
+
+
 def I_from_a0(a0: float, lambda0: float = 0.8e-6) -> float:
     E0 = E0_from_a0(a0, lambda0)
-    return 0.5 * E0**2 / mu_0 / c
+    return I_from_E0(E0)
+
+
+def a0_from_I(I: float, lambda0: float = 0.8e-6) -> float:
+    E0 = E0_from_I(I)
+    return a0_from_E0(E0, lambda0)
 
 
 def power(a0: float, w0: float, lambda0: float = 0.8e-6) -> float:
@@ -27,7 +36,22 @@ def power(a0: float, w0: float, lambda0: float = 0.8e-6) -> float:
     return 0.5 * pi * w0**2 * I
 
 
-def GaussianProfile(
+def E0_from_power(power: float, w0: float, lambda0: float = 0.8e-6) -> float:
+    I = 2*power / (pi*w0**2)
+    return E0_from_I(I)
+
+
+def a0_from_power(power: float, w0: float, lambda0: float = 0.8e-6) -> float:
+    E0 = E0_from_power(power, w0, lambda0)
+    return a0_from_E0(E0, lambda0)
+
+
+def w0_from_power(power: float, a0: float, lambda0: float = 0.8e-6) -> float:
+    I = I_from_a0(a0, lambda0)
+    return sqrt(power/I/(0.5*pi))
+
+
+def GaussianSpaceProfile(
     amp: float, w0: float, ctau: float, x0: Tuple[float, float, float],
     xf: float = None, lambda0: float = 0.8e-6, cep: float = 0.0
 ):
