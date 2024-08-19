edb63350a4cee3363505557d9be840332b49dbc9
gengxs <xsgeng@siom.ac.cn>
2024-08-19 16:26:01 +0800

    add gold_alpha to cmap

--- mytools/cmap.py
+++ mytools/cmap.py
@@ -24,6 +24,27 @@ bwr_alpha = _LinearSegmentedColormap(
         ],
     ), 
 )
+gold_alpha = _LinearSegmentedColormap(
+    'gold_alpha', 
+    dict(
+        red=[
+            (0, 1, 1), 
+            (1, 1, 1), 
+        ],
+        green=[
+            (0, 1, 1),
+            (1, 0.9, 1),
+        ],
+        blue=[
+            (0, 1, 1),
+            (1, 0, 1),
+        ],
+        alpha = [
+            (0, 0, 0),
+            (1, 1, 1)
+        ],
+    )
+)
 grey_alpha = _LinearSegmentedColormap(
     'grey_alpha', 
     dict(
