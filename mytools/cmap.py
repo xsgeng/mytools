308b60180e32ad7ea5d090e75d7e006661f41c57
gengxs <xsgeng@siom.ac.cn>
2024-10-24 10:34:00 +0800

    Add gwb_alpha colormap to cmap.py

--- mytools/cmap.py
+++ mytools/cmap.py
@@ -108,3 +108,29 @@ blue_alpha = _LinearSegmentedColormap(
         ],
     ), 
 )
+
+gwb_alpha = _LinearSegmentedColormap(
+    'gwb_alpha', 
+    dict(
+        red=[
+            (0,   0, 0.36), 
+            (0.5, 1, 1), 
+            (1,   0.96, 1)
+        ],
+        green=[
+            (0, 0, 0.75),
+            (0.5, 1, 1),
+            (1, 0.79, 0)
+        ],
+        blue=[
+            (0, 0, 0.75),
+            (0.5, 1, 1),
+            (1, 0.29, 0)
+        ],
+        alpha = [
+            (0, 1, 1),
+            (0.5, 0, 0),
+            (1, 1, 1)
+        ],
+    ), 
+)
